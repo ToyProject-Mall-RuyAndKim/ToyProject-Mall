a8d14a36922ccfa9e51d571973581155c5dddc87
kwj2435 <dmlwls2435@naver.com>
2020-09-20 12:38:13 +0900

    aws setting done

--- src/main/java/web/mall/ToyProjectFoodMallApplication.java
+++ src/main/java/web/mall/ToyProjectFoodMallApplication.java
@@ -2,9 +2,10 @@ package web.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToyProjectFoodMallApplication {
+public class ToyProjectFoodMallApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToyProjectFoodMallApplication.class, args);
